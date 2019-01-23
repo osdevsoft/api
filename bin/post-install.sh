36a5bfc58fcee56f1a555d2b2b811a6476facc82
xavi <xhernandez@leadtech.com>
2019-01-23 16:15:46 +0100

    post install

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -2,11 +2,11 @@
 
 #osds config
 #cp /var/www/osds/code/api/vendor/osds/api/src/Infrastructure/Symfony/config/packages/osds_api.yaml /var/www/osds/code/api/config/packages/
-cp ../src/Infrastructure/Symfony/config/packages/osds_api.yaml ././../../../../config/packages/
+cp ./vendor/osds/api/src/Infrastructure/Symfony/config/packages/osds_api.yaml ./config/packages/
 
 #nelmio documentation (enable /api/doc url)
 #cat /var/www/osds/code/api/vendor/osds/api/src/Infrastructure/Symfony/config/packages/nelmio_api_doc.yaml > /var/www/osds/code/api/config/packages/nelmio_api_doc.yaml
-cat ../src/Infrastructure/Symfony/config/packages/nelmio_api_doc.yaml > ././../../../../config/packages/nelmio_api_doc.yaml
+cat ./vendor/osds/api/src/Infrastructure/Symfony/config/packages/nelmio_api_doc.yaml > ./config/packages/nelmio_api_doc.yaml
 
 #cat /var/www/osds/code/api/vendor/osds/api/src/Infrastructure/Symfony/config/routes/nelmio_api_doc.yaml >> /var/www/osds/code/api/config/routes/nelmio_api_doc.yaml
-cat /var/www/osds/code/api/vendor/osds/api/src/Infrastructure/Symfony/config/routes/nelmio_api_doc.yaml >> ././../../../../config/routes/nelmio_api_doc.yaml
+cat ./vendor/osds/api/src/Infrastructure/Symfony/config/routes/nelmio_api_doc.yaml >> ./config/routes/nelmio_api_doc.yaml
