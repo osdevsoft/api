465bbb20d6439924202236d0e08b4c92d7f2b075
xavi <xhernandez@leadtech.com>
2019-01-23 15:54:06 +0100

    post install actions

--- bin/post-install.sh
+++ bin/post-install.sh
@@ -4,4 +4,5 @@
 cp ../src/Infrastructure/Symfony/config/packages/osds_api.yaml ../../../../config/packages/
 
 #nelmio documentation (enable /api/doc url)
-cat ../src/Infrastructure/Symfony/config/packages/nelmio_api_doc.yaml >> ../../../../config/packages/nelmio_api_doc.yaml
+cat ../src/Infrastructure/Symfony/config/packages/nelmio_api_doc.yaml > ../../../../config/packages/nelmio_api_doc.yaml
+cat ../src/Infrastructure/Symfony/config/routes/nelmio_api_doc.yaml >> ../../../../config/routes/nelmio_api_doc.yaml
